082406da4639d19d3bf6c96c139b80cc9727d74e
Rene Avila <0228505@up.edu.mx>
2023-04-26 18:35:36 -0600

    Features 2.0

--- features.py
+++ features.py
@@ -11,28 +11,28 @@ import matplotlib.pyplot as plt
 image = cv2.imread('./Graffiti/Graffiti_1.png')
 
 
-def featureRedHistogram(Image):
+def featureRedHistogram(image):
     red = np.zeros(255,dtype=int)
     for i in range(0,image.shape[0]):
         for j in range(0,image.shape[1]):
             red[int(image[i,j,2])-1] +=1
     return red
 
-def featureBlueHistogram(Image):
+def featureBlueHistogram(image):
     blue = np.zeros(255,dtype=int)
     for i in range(0,image.shape[0]):
         for j in range(0,image.shape[1]):
             blue[int(image[i,j,0])-1] +=1
     return blue
 
-def featureGreenHistogram(Image):
+def featureGreenHistogram(image):
     green = np.zeros(255,dtype=int)
     for i in range(0,image.shape[0]):
         for j in range(0,image.shape[1]):
             green[int(image[i,j,1])-1] +=1
     return green
 
-def featureRGBGMedia(Image):
+def featureRGBGMedia(image):
     #BGR order channel
     blue = 0
     green = 0
@@ -52,7 +52,7 @@ def featureRGBGMedia(Image):
     
     return [red,green,blue,grayScale]
 
-def featureHSIMedia():       
+def featureHSIMedia(image):       
     #HSV
     hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     size = image.shape[0]*image.shape[1]
@@ -69,6 +69,4 @@ def featureHSIMedia():
     value /= size
     return [hue,saturation,value]
 
-print(featureRedHistogram(image))
-print(featureBlueHistogram(image))
-print(featureGreenHistogram(image))
+print(featureHSIMedia(image))
